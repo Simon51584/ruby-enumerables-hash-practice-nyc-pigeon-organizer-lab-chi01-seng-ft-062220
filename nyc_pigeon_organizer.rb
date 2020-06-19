1fe36ba692c7515777b5f663d7e9e95515d29fff
Seth Starr <starrseth@hotmail.com>
2020-06-19 00:22:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ def nyc_pigeon_organizer(data)
  total_results = data.each_with_object({}) do |(key,value), f_array|
    value.each do |index, name|
      name.each do |inner_name|
-              if !f_array[inner_name]
-                f_array[inner_name] = {}
-                end 
+          if !f_array[inner_name]
+              f_array[inner_name] = {}
+              end 
           if !f_array[inner_name][key]
-            !f_array[inner_name][key] = []
-            end 
+             !f_array[inner_name][key] = []
+             end 
             #binding.pry 
           f_array[inner_name][key].push(index.to_s)
         end 
